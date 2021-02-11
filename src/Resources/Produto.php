182326ec9fb541e01b972d6e0911b4d70b2a3994
Vagner do Carmo <vluzrmos@gmail.com>
2021-02-10 23:47:13 -0300

    Add tipo nfe

--- src/Resources/Produto.php
+++ src/Resources/Produto.php
@@ -4,5 +4,8 @@ namespace Vluzrmos\Enotas\Resources;
 
 class Produto extends AbstractResource
 {
+    const TIPO_NFE_SERVICO = 0;
+    const TIPO_NFE_PRODUTO = 1;
+
     protected $endpoint = 'produtos';
 }
